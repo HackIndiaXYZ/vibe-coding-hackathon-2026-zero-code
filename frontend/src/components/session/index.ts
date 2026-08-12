24d7d92314556bf5a49c12eb42f76ca7a11ee464
ನಂದನ್ ಡಿ <154668653+Nandan-D14@users.noreply.github.com>
2026-08-13 01:42:31 +0530

    Delete frontend directory

--- frontend/src/components/session/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) 2026 Agentic Company. All rights reserved.
- * Proprietary and non-commercial use only.
- */
-
-export * from "./session-header";
-export * from "./session-landing-view";
-export * from "./chat-composer";
-export * from "./tool-picker";
